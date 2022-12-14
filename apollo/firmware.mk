ecd4d671d95e32e4828a28e719f70978e5a9cb30
goodmeow <aarunalr@nusantararom.org>
2022-12-14 08:19:32 +0000

    firmware: apollo checkout ebdd2f41ef8b34b4e5e2d8a1485287eb7509c331

---
+++ apollo/firmware.mk
@@ -0,0 +1,20 @@
+PRODUCT_COPY_FILES += \
+        vendor/xiaomi-firmware/apollo/firmware-update/abl.elf:install/firmware-update/abl.elf \
+        vendor/xiaomi-firmware/apollo/firmware-update/aop.mbn:install/firmware-update/aop.mbn \
+        vendor/xiaomi-firmware/apollo/firmware-update/BTFM.bin:install/firmware-update/BTFM.bin \
+        vendor/xiaomi-firmware/apollo/firmware-update/cmnlib64.mbn:install/firmware-update/cmnlib64.mbn \
+        vendor/xiaomi-firmware/apollo/firmware-update/cmnlib.mbn:install/firmware-update/cmnlib.mbn \
+        vendor/xiaomi-firmware/apollo/firmware-update/devcfg.mbn:install/firmware-update/devcfg.mbn \
+        vendor/xiaomi-firmware/apollo/firmware-update/dspso.bin:install/firmware-update/dspso.bin \
+        vendor/xiaomi-firmware/apollo/firmware-update/featenabler.mbn:install/firmware-update/featenabler.mbn \
+        vendor/xiaomi-firmware/apollo/firmware-update/hyp.mbn:install/firmware-update/hyp.mbn \
+        vendor/xiaomi-firmware/apollo/firmware-update/km4.mbn:install/firmware-update/km4.mbn \
+        vendor/xiaomi-firmware/apollo/firmware-update/NON-HLOS.bin:install/firmware-update/NON-HLOS.bin \
+        vendor/xiaomi-firmware/apollo/firmware-update/qupv3fw.elf:install/firmware-update/qupv3fw.elf \
+        vendor/xiaomi-firmware/apollo/firmware-update/storsec.mbn:install/firmware-update/storsec.mbn \
+        vendor/xiaomi-firmware/apollo/firmware-update/tz.mbn:install/firmware-update/tz.mbn \
+        vendor/xiaomi-firmware/apollo/firmware-update/uefi_sec.mbn:install/firmware-update/uefi_sec.mbn \
+        vendor/xiaomi-firmware/apollo/firmware-update/xbl_4.elf:install/firmware-update/xbl_4.elf \
+        vendor/xiaomi-firmware/apollo/firmware-update/xbl_5.elf:install/firmware-update/xbl_5.elf \
+        vendor/xiaomi-firmware/apollo/firmware-update/xbl_config_4.elf:install/firmware-update/xbl_config_4.elf \
+        vendor/xiaomi-firmware/apollo/firmware-update/xbl_config_5.elf:install/firmware-update/xbl_config_5.elf
